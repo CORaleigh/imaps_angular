e69f4f4a3821d29b4f3e3725dfc7f5562931c5d7
justingreco <justin.greco@raleighnc.gov>
2019-02-06 11:22:29 -0500

    added text labels for sketch tool

--- src/app/sketch-tool/sketch-tool.component.ts
+++ src/app/sketch-tool/sketch-tool.component.ts
@@ -15,17 +15,20 @@ export class SketchToolComponent implements OnInit {
   fillcolor:string = 'rgba(255, 0, 0, 0.5)';
   linecolor:string = 'rgba(255, 0, 0, 1.0)';
   width:number = 1;
+  label:string = '';
+  activeTool:string = '';
 
   _selectedOutlineColor:string;
   constructor(public shared:SharedService, private cpService: ColorPickerService) { }
   async initialize() {
     try {
-      const [SimpleFillSymbol, Graphic, GraphicsLayer, Sketch, Color] = await loadModules([
+      const [SimpleFillSymbol, Graphic, GraphicsLayer, Sketch, Color, TextSymbol] = await loadModules([
         'esri/symbols/SimpleFillSymbol',
         'esri/Graphic',
         'esri/layers/GraphicsLayer',
         "esri/widgets/Sketch",
-        "esri/Color"
+        "esri/Color",
+        "esri/symbols/TextSymbol"
       ]);
       
       let layer:esri.GraphicsLayer = new GraphicsLayer({title: 'sketchGraphics', listMode: 'hide'});
@@ -33,13 +36,31 @@ export class SketchToolComponent implements OnInit {
       let sketch:esri.Sketch = new Sketch({layer:layer, view: this._mapView, container:this.sketchEl.nativeElement});
       this.shared.sketchTool = sketch;
       sketch.watch('activeTool', tool => {
+        this.activeTool = tool;
         if (['point','polyline','polygon','circle','rectangle'].indexOf(tool) > -1) {
           
           this.shared.selectTool.reset();
         }
       });      
       sketch.on('create', (event) => {
-        if (event)
+        if (event.state === 'complete' && this.label.length > 0) {
+          let textSymbol = {
+            type: "text",  // autocasts as new TextSymbol()
+            color: "white",
+            haloColor: "black",
+            haloSize: "1px",
+            text: this.label,
+            xoffset: 3,
+            yoffset: 3,
+            font: {  // autocast as new Font()
+              size: 12,
+              family: "sans-serif",
+              weight: "bold"
+            }
+          };
+          layer.add(new Graphic({geometry:event.graphic.geometry, symbol:textSymbol}));
+          this.label = '';
+        }
 
         if (event.state === 'start') {
           event.graphic.symbol = {
